02fa5e500e5952056808187ebe47bef322b46bb2
Michele Spanu <michele.spanu@medunigraz.at>
2026-02-26 14:15:02 +0100

    Update pipeline.R

--- R/pipeline.R
+++ R/pipeline.R
@@ -28,10 +28,17 @@ run_complete_pipeline <- function(counts_file, tpm_file, metadata_file, gtf_file
                                  experiment_name, output_dir, species = "mouse",
                                  run_wgcna = TRUE, run_tf_analysis = TRUE,
                                  run_immune_analysis = TRUE, run_lincs_analysis = TRUE,
-                                 remove_samples = NULL, lfc_threshold = 1,color_volcano_up="#CA3433",color_volcano_down="#2B7CB6",genes_to_label=NULL,covariates = NULL, design_formula = NULL, contrast_string = NULL,
-                                 fdr_threshold = 0.05,point_size_volcano=4,label_size_volcano=5,n_labels_up=10,n_labels_down=10,gsea_custom_pathways=NULL,n_gsea_enrich_up=5,n_gsea_enrich_down=5, color_gsea_down="#2B7CB6", color_gsea_up="#CA3433",color_gsea_ns="#C5C6C7",run_ssgsea=TRUE, ssgsea_extra_pathways=NULL, ssgsea_n_boxplot_pathways = 20)
+                                 remove_samples = NULL, lfc_threshold = 1, 
+                                 color_volcano_up="#CA3433", color_volcano_down="#2B7CB6", genes_to_label=NULL, 
+                                 point_size_volcano=4,label_size_volcano=5,n_labels_up=10,n_labels_down=10,
+                                 covariates = NULL, design_formula = NULL, contrast_string = NULL,
+                                 fdr_threshold = 0.05, gsea_custom_pathways=NULL, n_gsea_enrich_up=5, n_gsea_enrich_down=5, 
+                                 color_gsea_down="#2B7CB6", color_gsea_up="#CA3433", color_gsea_ns="#C5C6C7",
+                                 run_ssgsea=TRUE, ssgsea_extra_pathways=NULL, ssgsea_n_boxplot_pathways = 20,
+                                 customed_pathways = NULL)
   {
-
+  # !!! Added customed_pathways arg !!!
+  
   # Start timing
   start_time <- Sys.time()
 
@@ -179,7 +186,8 @@ run_complete_pipeline <- function(counts_file, tpm_file, metadata_file, gtf_file
   # GSEA analysis
   gsea_results <- run_gsea_analysis(
     de_results = de_results$de_results$efit,
-    species = ifelse(species == "mouse", "MM", "HS")
+    species = ifelse(species == "mouse", "MM", "HS"),
+    customed_pathways = customed_pathways
   )
 
   # Extract gene sets and ranks once here
