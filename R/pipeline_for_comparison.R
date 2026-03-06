682e9867485e307ef11dbac2a68f7098d42d56a8
Michele Spanu <michele.spanu@medunigraz.at>
2026-03-06 14:45:52 +0100

    Update pipeline_for_comparison.R

--- R/pipeline_for_comparison.R
+++ R/pipeline_for_comparison.R
@@ -46,7 +46,7 @@ run_complete_comp_pipeline <- function(counts_file, tpm_file, metadata_file, gtf
                                  fdr_threshold = 0.05, point_size_volcano=4, label_size_volcano=5, n_labels_up=10, n_labels_down=10,
                                  gsea_custom_pathways=NULL, n_gsea_enrich_up=5, n_gsea_enrich_down=5, 
                                  color_gsea_down="#2B7CB6", color_gsea_up="#CA3433", color_gsea_ns="#C5C6C7", 
-                                 ssgsea_extra_pathways=NULL, ssgsea_n_boxplot_pathways = 20, customed_pathways)
+                                 ssgsea_extra_pathways=NULL, ssgsea_n_boxplot_pathways = 20, extra_pathways)
   {
 
   # Start timing
@@ -211,7 +211,7 @@ run_complete_comp_pipeline <- function(counts_file, tpm_file, metadata_file, gtf
     gsea_results[[group]] <- run_gsea_analysis(
       de_results = de_results[[group]]$de_results$efit,
       species = ifelse(species == "mouse", "MM", "HS"),
-      customed_pathways = customed_pathways
+      extra_pathways = extra_pathways
     )
 
       # Extract gene sets and ranks once here
