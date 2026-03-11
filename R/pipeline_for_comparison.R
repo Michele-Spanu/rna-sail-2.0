95d43428c8289eb9e5a3a19ca8deb2b1da07d235
Michele Spanu <michele.spanu@medunigraz.at>
2026-03-11 10:18:56 +0100

    Update pipeline_for_comparison.R

--- R/pipeline_for_comparison.R
+++ R/pipeline_for_comparison.R
@@ -118,7 +118,7 @@ run_complete_comp_pipeline <- function(counts_file, tpm_file, metadata_file, gtf
   # !!! Changed way palette is taken !!!
   pal <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(9, "Set1"))
   condition_colors <- setNames(
-    pal(n_levels),
+    pal(n_conditions),
     conditions
   )
   
