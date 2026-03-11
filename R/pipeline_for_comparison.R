c0f18e5fc88b8afdea845840c3bec99c93101eab
Michele Spanu <michele.spanu@medunigraz.at>
2026-03-11 09:59:47 +0100

    Update pipeline_for_comparison.R

--- R/pipeline_for_comparison.R
+++ R/pipeline_for_comparison.R
@@ -46,7 +46,7 @@ run_complete_comp_pipeline <- function(counts_file, tpm_file, metadata_file, gtf
                                  fdr_threshold = 0.05, point_size_volcano=4, label_size_volcano=5, n_labels_up=10, n_labels_down=10,
                                  gsea_custom_pathways=NULL, n_gsea_enrich_up=5, n_gsea_enrich_down=5, 
                                  color_gsea_down="#2B7CB6", color_gsea_up="#CA3433", color_gsea_ns="#C5C6C7", 
-                                 ssgsea_extra_pathways=NULL, ssgsea_n_boxplot_pathways = 20, extra_pathways)
+                                 ssgsea_extra_pathways=NULL, ssgsea_n_boxplot_pathways = 20, extra_pathways = NULL)
   {
 
   # Start timing
@@ -181,7 +181,8 @@ run_complete_comp_pipeline <- function(counts_file, tpm_file, metadata_file, gtf
       label_size        = label_size_volcano,
       n_labels_up       = n_labels_up,
       n_labels_down     = n_labels_down,
-      highlight_genes = genes_to_label
+      highlight_genes = genes_to_label,
+      title = paste("Volcano Plot - Differential Gene Expression", group, sep="\n")
     # color_ns keeps default "grey50", or you can add a color_volcano_ns arg as well
     )
 
